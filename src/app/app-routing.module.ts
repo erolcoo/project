c021e09453b0effb364b3e5c97fc3d83f98d7d00
erolcoo <erolco_ivanov@mail.bg>
2024-03-10 16:08:39 +0200

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { SportsgarageComponent } from './core/sportsgarage/sportsgarage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path:'sportgarage', component: SportsgarageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
